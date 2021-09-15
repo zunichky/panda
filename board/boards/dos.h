431f0533d0575569ecce852b1009faf592238c9d
Comma Device <device@comma.ai>
2021-09-15 15:43:49 +0200

    oops

--- board/boards/dos.h
+++ board/boards/dos.h
@@ -231,5 +231,6 @@ const board board_dos = {
   .set_ir_power = dos_set_ir_power,
   .set_phone_power = unused_set_phone_power,
   .set_clock_source_mode = dos_set_clock_source_mode,
-  .set_siren = dos_set_siren
+  .set_siren = dos_set_siren,
+  .read_som_gpio = dos_read_som_gpio
 };
